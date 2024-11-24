34fbef43318c15af555ec588e1f67f59ba0f0057
Rezwan <rezwan1768@gmail.com>
2024-11-23 22:32:23 -0500

    Set up devlopment environment

---
+++ config/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "../dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // Webpack resolves paths relative to the working directory (where you 
+      // run the command), not the directory of the configuration file.
+      template: "./src/template.html", 
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      // {
+      //     test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      //     type: 'asset/resource',
+      //   },
+    ],
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import test from "./test";
+import "./styles.css";
+
+const p = document.querySelector("p");
+p.textContent = test;
+const t = 0;
